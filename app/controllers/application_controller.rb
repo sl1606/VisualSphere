27f128b69fd2a29cc3c026fb8cd92b5e2335f2e8
sl1606 <linshiwei0415@gmail.com>
2024-10-31 17:05:49 -0500

    update assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :authenticate_user
+  def current_user
+    User.find(session[:user_id]) if session[:user_id].present?
+  end
+
+  def authenticate_user
+    redirect_to login_path if current_user.blank?
+  end
 end
