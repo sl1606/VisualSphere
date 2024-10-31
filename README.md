5a59be6513a5554327b917a1c65b072018458e2a
sl1606 <linshiwei0415@gmail.com>
2024-10-31 17:30:35 -0500

    quick update

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## Introduction
 VisualSphere is a web-based interactive visualization system that directly interfaces with clinical research data repositories, streamlining and simplifying the visualization workflow.
+
+## Get Started
+1. bundle install
+2. bundle exec rails server
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :dashboards
+  resources :charts do
+    collection do
+      get :get_chart_type
+    end
+  end
+  resources :collections, only: [:show, :destroy] do
+    collection do
+      get :get_variables
+      post :save_variables
+      put :update_variables
+      put :update_chart_type
+    end
+  end
+  resources :db_connections
+  get 'login', to: 'sessions#new'
+  delete 'logout', to: 'sessions#destroy'
+  root 'db_connections#index'
+  resources :sessions, only: [:new, :create]
+  resources :home, only: [:new, :create] do
+    collection do
+      get :success
+      get :fail
+      get :mysql_new
+      post :mysql_create
+    end
+  end
 end
